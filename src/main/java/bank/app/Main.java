e3c9ab1f4d6174910c3521f410d62f1af7167b81
LumiereIT103 <pathrickgianxdeguzman@gmail.com>
2026-08-14 22:38:35 +0800

    testing something

---
+++ src/main/java/bank/app/Main.java
@@ -0,0 +1,28 @@
+package bank.app;
+import bank.config.DBConnection;
+import java.sql.Connection;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        try (Connection connection = DBConnection.getConnection()) {
+//
+//            System.out.println("Database connection successful!");
+//
+//            System.out.println(
+//                    "Connected to: " +
+//                            connection.getMetaData().getDatabaseProductName()
+//            );
+//
+//            System.out.println(
+//                    "Database: " +
+//                            connection.getCatalog()
+//            );
+//
+//        } catch (Exception e) {
+//
+//            System.out.println("Database connection failed.");
+//            e.printStackTrace();
+//        }
+    }
+}
