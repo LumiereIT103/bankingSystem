eb311e301cdf85f4755b62c7854d7e8fb28b4b61
LumiereIT103 <pathrickgianxdeguzman@gmail.com>
2026-08-17 00:05:49 +0800

    feat(DAO): made the TransactionDAO and its implementation. might be subject to revision and it's not final.

---
+++ src/main/java/bank/dao/TransactionDAO.java
@@ -0,0 +1,29 @@
+package bank.dao;
+
+import bank.model.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionDAO {
+    Transaction createTransaction(Transaction transaction);
+
+    Optional<Transaction> findById(long transactionId);
+
+    Optional<Transaction> findByReferenceNumber(
+            String referenceNumber
+    );
+
+    List<Transaction> findByAccountId(
+            long accountId
+    );
+
+    List<Transaction> findByAccountIdOrderByDateDesc(
+            long accountId
+    );
+
+    List<Transaction> findRecentByAccountId(
+            long accountId,
+            int limit
+    );
+}
